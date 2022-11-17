dce2729f532b661bf575fe87c0826ba20e07e52e
kamilgaliev <kamil11122012@gmail.com>
2022-11-17 18:40:36 +0300

    Контроллер каталога товаров

--- WebStore/Infrastructure/Services/InMemoryProductData.cs
+++ WebStore/Infrastructure/Services/InMemoryProductData.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using WebStore.Data;
+using WebStore.Domain;
 using WebStore.Domain.Entities;
 using WebStore.Infrastructure.Interfaces;
 
@@ -10,5 +12,18 @@ namespace WebStore.Infrastructure.Services
         public IEnumerable<Brand> GetBrands() => TestData.Brands;
 
         public IEnumerable<Section> GetSections() => TestData.Sections;
+
+        public IEnumerable<Product> GetProducts(ProductFilter filter)
+        {
+            var query = TestData.Products;
+
+            if (filter?.SectionId is { } section_id)
+                query = query.Where(s => s.SectionId == section_id);
+
+            if (filter?.BrandId is { } brand_id)
+                query = query.Where(b => b.BrandId == brand_id);
+
+            return query;
+        }
     }
 }

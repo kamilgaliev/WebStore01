758a182b42165ada90b80449d3e65188597a3774
kamilgaliev <kamil11122012@gmail.com>
2022-12-29 15:48:40 +0300

    Загатовка клнтроллера Account

---
+++ WebStore/Controllers/AccountController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.Domain.Entities.Identity;
+
+namespace WebStore.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<User> _UserManager;
+        private readonly RoleManager<Role> _RoleManager;
+
+        public AccountController(UserManager<User> UserManager, RoleManager<Role> RoleManager)
+        {
+            _UserManager = UserManager;
+            _RoleManager = RoleManager;
+        }
+    }
+}

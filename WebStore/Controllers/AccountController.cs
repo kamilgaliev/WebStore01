5e5af874ece27b09a0bfe2b4f24f978e606dfc99
kamilgaliev <kamil11122012@gmail.com>
2022-12-29 16:35:50 +0300

    Регистрация пользователя - контроллер Account

--- WebStore/Controllers/AccountController.cs
+++ WebStore/Controllers/AccountController.cs
@@ -1,18 +1,47 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 using WebStore.Domain.Entities.Identity;
+using WebStore.ViewModels;
 
 namespace WebStore.Controllers
 {
     public class AccountController : Controller
     {
         private readonly UserManager<User> _UserManager;
-        private readonly RoleManager<Role> _RoleManager;
+        private readonly SignInManager<User> _SignInManager;
 
-        public AccountController(UserManager<User> UserManager, RoleManager<Role> RoleManager)
+        public AccountController(UserManager<User> UserManager, SignInManager<User> SignInManager)
         {
             _UserManager = UserManager;
-            _RoleManager = RoleManager;
+            _SignInManager = SignInManager;
+        }
+
+        public IActionResult Register() => View(new RegisterUserViewModel());
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterUserViewModel Model)
+        {
+            if(!ModelState.IsValid) return View(Model);
+
+            var user = new User
+            {
+                UserName = Model.UserName
+            };
+
+            var registration_reuslt = await _UserManager.CreateAsync(user, Model.Password);
+
+            if(registration_reuslt.Succeeded)
+            {
+                await _SignInManager.SignInAsync(user, false);
+
+                return RedirectToAction("Index", "Home");
+            }
+
+            foreach (var error in registration_reuslt.Errors)
+                ModelState.AddModelError("", error.Description);
+
+            return View(Model);
         }
     }
 }

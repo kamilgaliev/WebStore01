77068cabd03909c2ffa4aa254fa761abf9bff62b
kamilgaliev <kamil11122012@gmail.com>
2023-02-09 15:23:51 +0300

    Базовый клиент

---
+++ Services/WebStore.Clients/Base/BaseClient.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebStore.Clients.Base
+{
+    public abstract class BaseClient
+    {
+        protected string Address { get; }
+
+        protected HttpClient Http { get; }
+
+        protected BaseClient(IConfiguration Configuration, string ServiceAddress)
+        {
+            Address = ServiceAddress;
+
+            Http = new HttpClient 
+            { 
+                BaseAddress = new Uri(Configuration["WebApiURL"]),
+                DefaultRequestHeaders =
+                {
+                    Accept = {new MediaTypeWithQualityHeaderValue("application/json")}
+                }
+            };
+        }
+    }
+}

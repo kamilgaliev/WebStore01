2bc0f1a92a33fa0623a9ed5c307f2c6effd26fcd
kamilgaliev <kamil11122012@gmail.com>
2023-02-10 16:33:30 +0300

    Контроллер WebApi для управления сотрудниками

---
+++ Services/WebStore.ServiceHosting/Controllers/EmployeesApiController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using WebStore.Domain.Models;
+using WebStore.Interfaces;
+
+namespace WebStore.ServiceHosting.Controllers
+{
+    [Route("api/employees")]
+    [ApiController]
+    public class EmployeesApiController : ControllerBase, IEmployeesData
+    {
+        private readonly IEmployeesData _EmployeesData;
+
+        public EmployeesApiController(IEmployeesData EmployeesData)
+        {
+            _EmployeesData = EmployeesData;
+        }
+
+        [HttpPost]
+        public int Add(Employee employee)
+        {
+            return _EmployeesData.Add(employee);
+        }
+
+        [HttpDelete("{id}")]
+        public bool Delete(int id)
+        {
+            return _EmployeesData.Delete(id);
+        }
+
+        [HttpGet]
+        public IEnumerable<Employee> Get()
+        {
+            return _EmployeesData.Get();
+        }
+
+        [HttpGet("{id}")]
+        public Employee Get(int id)
+        {
+            return _EmployeesData.Get(id);
+        }
+
+        [HttpPut]
+        public void Update(Employee employee)
+        {
+            _EmployeesData.Update(employee);
+        }
+    }
+}

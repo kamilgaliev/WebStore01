29bae55ae9233868d01d19c43666ff4cdff8f05a
kamilgaliev <kamil11122012@gmail.com>
2023-02-10 16:12:58 +0300

    Переносим подключение к БД в WebStore.ServiceHosting

--- Services/WebStore.ServiceHosting/Startup.cs
+++ Services/WebStore.ServiceHosting/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -10,6 +11,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebStore.DAL.Context;
+using WebStore.Services.Data;
 
 namespace WebStore.ServiceHosting
 {
@@ -24,6 +27,10 @@ namespace WebStore.ServiceHosting
 
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<WebStoreDB>(opt =>
+                opt.UseSqlServer(Configuration.GetConnectionString("Default")));
+
+            services.AddTransient<WebStoreDbInitializer>();
 
             services.AddControllers();
             services.AddSwaggerGen(c =>

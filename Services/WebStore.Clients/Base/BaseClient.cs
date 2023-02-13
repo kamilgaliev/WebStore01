5681e3a79908d7fb369b208c63f1f7475957e7e6
kamilgaliev <kamil11122012@gmail.com>
2023-02-13 15:33:45 +0300

    Доработка базового клиента

--- Services/WebStore.Clients/Base/BaseClient.cs
+++ Services/WebStore.Clients/Base/BaseClient.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace WebStore.Clients.Base
 {
-    public abstract class BaseClient
+    public abstract class BaseClient : IDisposable
     {
         protected string Address { get; }
 
@@ -27,6 +27,64 @@ namespace WebStore.Clients.Base
                     Accept = {new MediaTypeWithQualityHeaderValue("application/json")}
                 }
             };
+        }
+
+        protected T Get<T> (string url) => GetAsync<T> (url).Result;
+
+        protected async Task<T> GetAsync<T>( string url)
+        {
+            var response = await Http.GetAsync(url);
+
+            return await response.EnsureSuccessStatusCode().Content.ReadAsAsync<T>();
+        }
+
+        protected HttpResponseMessage Post<T> (string url, T item) => PostAsync(url, item).Result;
+
+        protected async Task<HttpResponseMessage> PostAsync<T> (string url, T item) 
+        {
+            var response = await Http.PostAsJsonAsync(url, item);
+
+            return response.EnsureSuccessStatusCode();
+        }
+
+        protected HttpResponseMessage Put<T>(string url, T item) => PutAsync(url, item).Result;
+
+        protected async Task<HttpResponseMessage> PutAsync<T>(string url, T item)
+        {
+            var response = await Http.PutAsJsonAsync(url, item);
+
+            return response.EnsureSuccessStatusCode();
+        }
+
+        protected HttpResponseMessage Delete(string url) => DeleteAsync(url).Result;
+
+        protected async Task<HttpResponseMessage> DeleteAsync (string url)
+        {
+            var response = await Http.DeleteAsync(url);
+
+            return response;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            //GC.SuppressFinalize(this); // при наличии ~BaseClient() => Dispose(false);
+        }
+
+        //~BaseClient() => Dispose(false);
+
+        private bool _Disposed;
+        protected virtual void Dispose(bool disposing)
+        {
+            if(_Disposed) return;
+
+            if(disposing)
+            {
+                //очистка управляемых ресурсов
+                Http.Dispose();
+            }
+            //очистка неуправляемых ресурсов
+            _Disposed = true;
         }
     }
 }

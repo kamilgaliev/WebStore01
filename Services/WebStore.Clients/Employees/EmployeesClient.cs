a651121fa3ba4695135f51a03e46b24bd155c7ea
kamilgaliev <kamil11122012@gmail.com>
2023-02-14 17:07:15 +0300

    Доработка клиента сотрудников - добавлено логирование

--- Services/WebStore.Clients/Employees/EmployeesClient.cs
+++ Services/WebStore.Clients/Employees/EmployeesClient.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,13 +14,23 @@ namespace WebStore.Clients.Employees
 {
     public class EmployeesClient : BaseClient, IEmployeesData
     {
-        public EmployeesClient(IConfiguration Configuration) : base(Configuration, WebAPI.Employees)
+        private readonly ILogger<EmployeesClient> _Logger;
+
+        public EmployeesClient(IConfiguration Configuration,ILogger<EmployeesClient> Logger) : base(Configuration, WebAPI.Employees)
         {
+            _Logger = Logger;
         }
 
         public int Add(Employee employee) => Post(Address, employee).Content.ReadAsAsync<int>().Result;
 
-        public bool Delete(int id) => Delete($"{Address}/{id}").IsSuccessStatusCode;
+        public bool Delete(int id)
+        {
+            _Logger.LogInformation("Удаление сотрудника id: {0} ...", id);
+            var result = Delete($"{Address}/{id}").IsSuccessStatusCode;
+            _Logger.LogInformation("Удаление сотрудника id: {0} - {1}", id, result ? "выполнено" : "не найден");
+
+            return result;
+        }
 
         public IEnumerable<Employee> Get() => Get<IEnumerable<Employee>>(Address);
 
--- Services/WebStore.ServiceHosting/Controllers/EmployeesApiController.cs
+++ Services/WebStore.ServiceHosting/Controllers/EmployeesApiController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using System.Collections.Generic;
 using WebStore.Domain.Models;
 using WebStore.Interfaces;
@@ -11,22 +12,29 @@ namespace WebStore.ServiceHosting.Controllers
     public class EmployeesApiController : ControllerBase, IEmployeesData
     {
         private readonly IEmployeesData _EmployeesData;
+        private readonly ILogger<EmployeesApiController> _Logger;
 
-        public EmployeesApiController(IEmployeesData EmployeesData)
+        public EmployeesApiController(IEmployeesData EmployeesData, ILogger<EmployeesApiController> Logger)
         {
             _EmployeesData = EmployeesData;
+            _Logger = Logger;
         }
 
         [HttpPost]
         public int Add(Employee employee)
         {
+            _Logger.LogInformation("Добавление сотрудника {0}", employee);
             return _EmployeesData.Add(employee);
         }
 
         [HttpDelete("{id}")]
         public bool Delete(int id)
         {
-            return _EmployeesData.Delete(id);
+            _Logger.LogInformation("Удаление сотрудника id: {0} ...", id);
+            var result = _EmployeesData.Delete(id);
+            _Logger.LogInformation("Удаление сотрудника id: {0} - {1}", id, result ? "выполнено" : "не найден");
+
+            return result;
         }
 
         [HttpGet]
@@ -44,6 +52,7 @@ namespace WebStore.ServiceHosting.Controllers
         [HttpPut]
         public void Update(Employee employee)
         {
+            _Logger.LogInformation("Редактирование сотрудника {0}", employee);
             _EmployeesData.Update(employee);
         }
     }

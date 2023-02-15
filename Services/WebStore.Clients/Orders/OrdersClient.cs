ad119766ef10404b22070b532cc396a2ca99226a
kamilgaliev <kamil11122012@gmail.com>
2023-02-15 19:28:09 +0300

    Клиент WebAPI сервиса заказов

---
+++ Services/WebStore.Clients/Orders/OrdersClient.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using WebStore.Clients.Base;
+using WebStore.Domain.DTO;
+using WebStore.Interfaces;
+
+namespace WebStore.Clients.Orders
+{
+    public class OrdersClient : BaseClient, IOrderService
+    {
+        public OrdersClient(IConfiguration Configuration) : base(Configuration, WebAPI.Orders)
+        {
+        }
+
+        public async Task<OrderDTO> CreateOrder(string UserName, CreateOrderModel OrderModel)
+        {
+            var response = await PostAsync($"{Address}/{UserName}", OrderModel);
+
+            return await response.Content.ReadAsAsync<OrderDTO>();
+        }
+
+        public async Task<OrderDTO> GetOrderById(int id) => await GetAsync<OrderDTO>($"{Address}/{id}");
+
+        public async Task<IEnumerable<OrderDTO>> GetUserOrders(string UserName) => 
+            await GetAsync<IEnumerable<OrderDTO>>($"{Address}/user/{UserName}");
+    }
+}
--- UI/WebStore/Startup.cs
+++ UI/WebStore/Startup.cs
@@ -18,6 +18,7 @@ using WebStore.Interfaces.TestAPI;
 using WebStore.Clients.Values;
 using WebStore.Clients.Employees;
 using WebStore.Clients.Products;
+using WebStore.Clients.Orders;
 
 namespace WebStore
 {
@@ -44,7 +45,8 @@ namespace WebStore
             services.AddTransient<IEmployeesData, EmployeesClient>();
 
             services.AddTransient<ICartService, InCookiesCartService>();
-            services.AddTransient<IOrderService, SqlOrderService>();
+            //services.AddTransient<IOrderService, SqlOrderService>();
+            services.AddTransient<IOrderService, OrdersClient>();
 
             services.AddTransient<IValuesService,ValuesClient>();
 

eeb7424f2b8b9cd9aa6c66ff74380af8c73b648c
kamilgaliev <kamil11122012@gmail.com>
2023-02-17 17:19:30 +0300

    Исправление ошибки

--- UI/WebStore/Controllers/CartController.cs
+++ UI/WebStore/Controllers/CartController.cs
@@ -61,6 +61,7 @@ namespace WebStore.Controllers
                 Order = OrderModel,
                 Items = _CartService.GetViewModel().Items.Select(item => new OrderItemDTO
                 {
+                    Id = item.Product.Id,
                     Price = item.Product.Price,
                     Quantity = item.Quantity,
                 }).ToList(),
